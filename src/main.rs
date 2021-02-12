bab4865d5c82795e9575f257f87e5a6a1923a230
Marcel Robitaille <mail@marcelrobitaille.me>
2021-02-11 22:27:14 -0500

    Attempt to use config file for disk usage
    Keyword: attempt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use bytesize::ByteSize;
+use itertools::Itertools;
 use serde::Deserialize;
 use std::collections::HashMap;
 use std::fs;
+use std::hash::Hash;
 use std::path::Path;
-use systemstat::{Platform, System};
+use systemstat::{Filesystem, Platform, System};
 use termion::{color, style};
 
 // TODO: Move config to it's own file
@@ -49,38 +51,57 @@ fn main() {
     match fs::read_to_string("default_config.toml") {
         Ok(config_str) => {
             let config: Config = toml::from_str(&config_str).unwrap();
+            let sys = System::new();
+
+            let mounts = sys.mounts();
+            let mounts: HashMap<&str, &Filesystem> = mounts
+                .iter()
+                .group_by(|x| &x.into_iter().nth(0).unwrap().fs_mounted_on[..])
+                .into_iter()
+                .map(|(k, mut v)| (k, v.next().unwrap().into_iter().nth(0).unwrap()))
+                .collect();
             println!("{:?}", config);
+
+            for (filesystem_name, mount_point) in config.filesystems.unwrap().iter() {
+                println!("{}: {}", filesystem_name, mount_point);
+            }
+            println!("{:?}", mounts);
         }
         Err(e) => println!("Error reading config file: {}", e),
     }
 
-    let sys = System::new();
+    // match sys.mounts() {
+    //     Ok(mounts) => {
+    //         for mount in mounts.iter() {
+    //         }
+    //     }
+    //     Err(x) => println!("\nMounts: error: {}", x),
+    // }
 
-    match sys.mount_at(Path::new("/")) {
-        Ok(mount) => {
-            let total = mount.total.as_u64();
-            let avail = mount.avail.as_u64();
-            let used = total - avail;
-            let bar_full = BAR_WIDTH * used / total;
-            let bar_empty = BAR_WIDTH - bar_full;
+    // match sys.mount_at(Path::new("/")) {
+    //     Ok(mount) => {
+    //         let total = mount.total.as_u64();
+    //         let avail = mount.avail.as_u64();
+    //         let used = total - avail;
+    //         let bar_full = BAR_WIDTH * used / total;
+    //         let bar_empty = BAR_WIDTH - bar_full;
 
-            println!(
-                "{} -> {} ({}) {}/{}",
-                mount.fs_mounted_from,
-                mount.fs_mounted_on,
-                mount.fs_type,
-                ByteSize::b(used),
-                ByteSize::b(total)
-            );
-            println!(
-                "[{}{}{}{}{}]",
-                color::Fg(color::Green),
-                "=".repeat(bar_full as usize),
-                color::Fg(color::LightBlack),
-                "=".repeat(bar_empty as usize),
-                style::Reset,
-            );
-        }
-        Err(x) => println!("\nMounts: error: {}", x),
-    }
+    //         println!(
+    //             "{} -> {} ({}) {}/{}",
+    //             mount.fs_mounted_from,
+    //             mount.fs_mounted_on,
+    //             mount.fs_type,
+    //             ByteSize::b(used),
+    //             ByteSize::b(total)
+    //         );
+    //         println!(
+    //             "[{}{}{}{}{}]",
+    //             color::Fg(color::Green),
+    //             "=".repeat(bar_full as usize),
+    //             color::Fg(color::LightBlack),
+    //             "=".repeat(bar_empty as usize),
+    //             style::Reset,
+    //         );
+    //     }
+    // }
 }
