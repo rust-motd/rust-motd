0ed0f57ff0e6ea8c1d40c2cf98baef0a4eedfb2f
Aaron Gross <aaron.gross.code@gmail.com>
2021-02-11 20:49:10 -0500

    Finish config file parsing, add example config file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use std::path::Path;
 use bytesize::ByteSize;
-use systemstat::{System, Platform};
-use termion::{color, style};
 use serde::Deserialize;
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
+use systemstat::{Platform, System};
+use termion::{color, style};
 
+// TODO: Move config to it's own file
 #[derive(Debug, Deserialize)]
 struct Config {
     ascii_text_art: Option<Ata>,
-
+    service_status: Option<HashMap<String, String>>,
+    uptime: Option<Uptime>,
+    ssl_certificates: Option<SSLCerts>,
+    filesystems: Option<HashMap<String, String>>,
+    fail_2_ban: Option<Fail2Ban>,
+    last_login: Option<HashMap<String, usize>>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -17,11 +25,35 @@ struct Ata {
     command: String,
 }
 
+#[derive(Debug, Deserialize)]
+struct Uptime {
+    prefix: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct SSLCerts {
+    sort_method: String, // TODO: Maybe switch to enum insead of string
+    // need to figure out how to do this in Serde
+    certs: HashMap<String, String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Fail2Ban {
+    jails: Vec<String>,
+}
+
 const LINE_WIDTH: u64 = 60;
 const BAR_WIDTH: u64 = LINE_WIDTH - 2;
 
-
 fn main() {
+    match fs::read_to_string("default_config.toml") {
+        Ok(config_str) => {
+            let config: Config = toml::from_str(&config_str).unwrap();
+            println!("{:?}", config);
+        }
+        Err(e) => println!("Error reading config file: {}", e),
+    }
+
     let sys = System::new();
 
     match sys.mount_at(Path::new("/")) {
@@ -32,12 +64,14 @@ fn main() {
             let bar_full = BAR_WIDTH * used / total;
             let bar_empty = BAR_WIDTH - bar_full;
 
-            println!("{} -> {} ({}) {}/{}",
-                     mount.fs_mounted_from,
-                     mount.fs_mounted_on,
-                     mount.fs_type,
-                     ByteSize::b(used),
-                     ByteSize::b(total));
+            println!(
+                "{} -> {} ({}) {}/{}",
+                mount.fs_mounted_from,
+                mount.fs_mounted_on,
+                mount.fs_type,
+                ByteSize::b(used),
+                ByteSize::b(total)
+            );
             println!(
                 "[{}{}{}{}{}]",
                 color::Fg(color::Green),
@@ -47,6 +81,6 @@ fn main() {
                 style::Reset,
             );
         }
-        Err(x) => println!("\nMounts: error: {}", x)
+        Err(x) => println!("\nMounts: error: {}", x),
     }
 }
