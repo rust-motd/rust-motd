3806dc941aa720da107e4cc00a3f8d3f08e89326
Aaron Gross <aaron.gross.code@gmail.com>
2021-03-02 22:33:40 -0500

    Add other colors to banner

--- src/components/banner.rs
+++ src/components/banner.rs
@@ -5,8 +5,44 @@ use thiserror::Error;
 
 #[derive(Debug, Deserialize)]
 pub struct BannerCfg {
-    color: String,
+    color: BannerColor,
     command: String,
+}
+
+#[derive(Debug, Deserialize)]
+enum BannerColor {
+    #[serde(alias = "black")]
+    Black,
+    #[serde(alias = "red")]
+    Red,
+    #[serde(alias = "green")]
+    Green,
+    #[serde(alias = "yellow")]
+    Yellow,
+    #[serde(alias = "blue")]
+    Blue,
+    #[serde(alias = "magenta")]
+    Magenta,
+    #[serde(alias = "cyan")]
+    Cyan,
+    #[serde(alias = "white")]
+    White,
+    #[serde(alias = "light_black")]
+    LightBlack,
+    #[serde(alias = "light_red")]
+    LightRed,
+    #[serde(alias = "light_green")]
+    LightGreen,
+    #[serde(alias = "light_yellow")]
+    LightYellow,
+    #[serde(alias = "light_blue")]
+    LightBlue,
+    #[serde(alias = "light_magenta")]
+    LightMagenta,
+    #[serde(alias = "light_cyan")]
+    LightCyan,
+    #[serde(alias = "light_white")]
+    LightWhite,
 }
 
 #[derive(Error, Debug)]
@@ -31,9 +67,28 @@ pub fn disp_banner(config: BannerCfg) -> Result<(), BannerError> {
         });
     }
 
+    let banner_color = match config.color {
+        BannerColor::Black => color::Black.fg_str(),
+        BannerColor::Red => color::Red.fg_str(),
+        BannerColor::Yellow => color::Yellow.fg_str(),
+        BannerColor::Green => color::Green.fg_str(),
+        BannerColor::Blue => color::Blue.fg_str(),
+        BannerColor::Magenta => color::Magenta.fg_str(),
+        BannerColor::Cyan => color::Cyan.fg_str(),
+        BannerColor::White => color::White.fg_str(),
+        BannerColor::LightBlack => color::LightBlack.fg_str(),
+        BannerColor::LightRed => color::LightRed.fg_str(),
+        BannerColor::LightYellow => color::LightYellow.fg_str(),
+        BannerColor::LightGreen => color::LightGreen.fg_str(),
+        BannerColor::LightBlue => color::LightBlue.fg_str(),
+        BannerColor::LightMagenta => color::LightMagenta.fg_str(),
+        BannerColor::LightCyan => color::LightCyan.fg_str(),
+        BannerColor::LightWhite => color::LightWhite.fg_str(),
+    };
+
     println!(
         "{}{}{}",
-        color::Fg(color::Red),
+        banner_color,
         &String::from_utf8_lossy(&output.stdout).trim_end(),
         style::Reset
     );
