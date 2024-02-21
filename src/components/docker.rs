aa10d1f943707f59af6570327948a572634b967c
GDWR <gregory.dwr@gmail.com>
2024-02-21 18:23:48 +0000

    Add docker compose configurations

--- src/components/docker.rs
+++ src/components/docker.rs
@@ -1,8 +1,8 @@
 use crate::constants::INDENT_WIDTH;
 use async_trait::async_trait;
+use itertools::Itertools;
 use serde::Deserialize;
-use docker_api::models::ContainerSummary;
-use docker_api::opts::ContainerListOpts;
+use docker_api::opts::{ContainerFilter, ContainerListOpts};
 use docker_api::{Docker as DockerAPI, Result as DockerResult};
 use std::collections::HashMap;
 use termion::{color, style};
@@ -14,15 +14,48 @@ use crate::default_prepare;
 #[derive(Debug, Deserialize)]
 pub struct Docker {
     containers: HashMap<String, String>,
+    composes: HashMap<String, String>,
 }
 
 #[async_trait]
 impl Component for Docker {
     async fn print(self: Box<Self>, _global_config: &GlobalConfig, _width: Option<usize>) {
         println!("Docker:");
-        self.print_or_error()
-            .await
-            .unwrap_or_else(|err| println!("Docker status error: {}", err));
+        let docker = match new_docker() {
+            Ok(docker) => docker,
+            Err(err) => {
+                println!("{indent}Error: {err}", err = err, indent = " ".repeat(INDENT_WIDTH));
+                return;
+            }
+        };
+
+        if !self.containers.is_empty() {
+            println!("{indent}Containers:", indent = " ".repeat(INDENT_WIDTH));
+            self.print_or_error_containers(&docker)
+                .await
+                .unwrap_or_else(|err| println!(
+                    "{indent}{red}Error getting containers: {err}{reset}",
+                    indent = " ".repeat(INDENT_WIDTH),
+                    red = color::Fg(color::Red),
+                    reset = style::Reset,
+                    err = err,
+                ));
+            println!();
+        }
+
+        if !self.composes.is_empty() {
+            println!("{indent}Composes:", indent = " ".repeat(INDENT_WIDTH));
+            self.print_or_error_composes(&docker)
+                .await
+                .unwrap_or_else(|err| println!(
+                    "{indent}{red}Error getting composes: {err}{reset}",
+                    indent = " ".repeat(INDENT_WIDTH),
+                    red = color::Fg(color::Red),
+                    reset = style::Reset,
+                    err = err,
+                ));
+            println!();
+        }
         println!();
     }
     default_prepare!();
@@ -38,80 +71,100 @@ pub fn new_docker() -> DockerResult<DockerAPI> {
     DockerAPI::new("tcp://127.0.0.1:8080")
 }
 
-struct Container {
-    summary: ContainerSummary,
-    name: String,
+fn status_to_color(status: &str) -> String {
+    match status {
+        "created" | "restarting" | "paused" | "removing" | "configured" => color::Fg(color::Yellow).to_string(),
+        "running" => color::Fg(color::Green).to_string(),
+        "exited" => color::Fg(color::LightBlack).to_string(),
+        "dead" => color::Fg(color::Red).to_string(),
+        _ => color::Fg(color::White).to_string(),
+    }
 }
 
-impl Docker {
-    pub async fn print_or_error(mut self) -> Result<(), Box<dyn std::error::Error>> {
-        let docker = new_docker()?;
-
-        // Get all containers from library and then filter them
-        // Not perfect, but I got strange issues when trying to use `.get(id)`
-        let containers: Vec<Container> = docker
-            .containers()
-            .list(&ContainerListOpts::builder().all(true).build())
-            .await?
-            .into_iter()
-            .filter_map(|container| {
-                match container.names.as_ref() {
-                    Some(names) => names.iter().find_map(|name| {
-                        self.containers
-                            .remove_entry(name)
-                            .map(|(_docker_name, display_name)| Container {
-                                name: display_name,
-                                summary: container.clone(),
-                            })
-                    }),
-                    _ => None,
-                }
-                // container.names.as_ref().map(|names| {
-                //     names.iter().find_map(|name| {
-                //         self.containers.remove_entry(name).map(|(_docker_name, display_name)| (display_name, container))
-                //     })
-                // })
-            })
-            .collect();
-
-        for (docker_name, _display_name) in self.containers {
-            println!(
-                "{indent}{color}Warning: Could not find Docker container `{docker_name}'{reset}",
-                indent = " ".repeat(INDENT_WIDTH),
-                color = color::Fg(color::Yellow),
-                docker_name = docker_name,
-                reset = style::Reset
-            );
-        }
-
-        // Max length of all the container names (first column)
-        // to determine the padding
-        if let Some(max_len) = containers
-            .iter()
-            .map(|container| container.name.len())
+impl Docker {    
+    async fn print_or_error_containers(&self, docker: &DockerAPI) -> Result<(), Box<dyn std::error::Error>> {
+        let longest_container_name = self.containers.values()
+            .map(|n| n.len())
             .max()
-        {
-            for container in containers {
-                let status_color = match container.summary.state.as_deref() {
-                    Some("Created") | Some("Restarting") | Some("Paused") | Some("Removing")
-                    | Some("Configured") => color::Fg(color::Yellow).to_string(),
-                    Some("Running") => color::Fg(color::Green).to_string(),
-                    Some("Exited") => color::Fg(color::LightBlack).to_string(),
-                    Some("Dead") => color::Fg(color::Red).to_string(),
-                    _ => color::Fg(color::White).to_string(),
-                };
-                println!(
-                    "{indent}{name}: {padding}{color}{status}{reset}",
-                    indent = " ".repeat(INDENT_WIDTH),
-                    name = container.name,
-                    padding = " ".repeat(max_len - container.name.len()),
-                    color = status_color,
-                    status = container.summary.status.unwrap_or(String::from("?")),
-                    reset = style::Reset,
-                );
+            .unwrap_or(0);
+
+        for (container_name, name) in self.containers.iter() {
+            let container = docker.containers()
+                .list(&ContainerListOpts::builder()
+                    .all(true)
+                    .filter([ContainerFilter::Name(container_name.to_string())])
+                    .build())
+                .await?;
+            
+            
+            match container.first() {
+                Some(container) => {
+                    let status = container.state.clone().unwrap_or("unknown".to_owned());
+
+                    println!(
+                        "{indent}{name}: {padding}{color}{status}{reset}",
+                        indent = " ".repeat(INDENT_WIDTH*2),
+                        name = name,
+                        padding = " ".repeat(longest_container_name - name.len()),
+                        color = status_to_color(&status),
+                        status = status,
+                        reset = style::Reset,
+                    );
+                },
+                None => {
+                    println!(
+                        "{indent}{name}: {padding}{color}Not found '{container_name}'{reset}",
+                        indent = " ".repeat(INDENT_WIDTH*2),
+                        container_name = container_name,
+                        name = name,
+                        padding = " ".repeat(longest_container_name - name.len()),
+                        color = color::Fg(color::Yellow),
+                        reset = style::Reset,
+                    );
+                }
             }
         }
 
+        Ok(())
+    }
+
+    async fn print_or_error_composes(&self, docker: &DockerAPI) -> Result<(), Box<dyn std::error::Error>> {
+        for (compose, friendly_name) in self.composes.iter(){ 
+            let compose_containers = docker.containers()
+                .list(&ContainerListOpts::builder()
+                    .all(true)
+                    .filter([ContainerFilter::Label("com.docker.compose.project".to_string(), compose.to_string())])
+                    .build())
+                .await?;
+
+            if compose_containers.is_empty() {
+                println!(
+                    "{indent}{name}: {color}Not found{reset}",
+                    indent = " ".repeat(INDENT_WIDTH*2),
+                    name = friendly_name,
+                    color = color::Fg(color::Yellow),
+                    reset = style::Reset,
+                );
+                continue;
+            }
+
+            let status_grouped_containers = compose_containers.iter()
+                .map(|container| {
+                    (container.state.clone().unwrap_or("unknown".to_owned()), container)
+                })
+                .into_group_map();
+
+            println!(
+                "{indent}{name}: {running_color}running({running}) {exited_color}exited({exited}){reset}",
+                indent = " ".repeat(INDENT_WIDTH*2),
+                name = friendly_name,
+                running_color = status_to_color("running"),
+                running = status_grouped_containers.get("running").map(|v| v.len()).unwrap_or(0),
+                exited_color = status_to_color("exited"),
+                exited = status_grouped_containers.get("exited").map(|v| v.len()).unwrap_or(0),
+                reset = style::Reset,
+            );
+        }
         Ok(())
     }
 }
